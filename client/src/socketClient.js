90d9c5344e2cc89ac7c3d4e9a07e74334c6e6f34
BorislavBranimirov <boriintorr@gmail.com>
2020-09-27 13:28:03 +0300

    fixed socket connection in dev mode

--- client/src/socketClient.js
+++ client/src/socketClient.js
@@ -1,4 +1,8 @@
-export const socket = io();
+// if running locally, connect to the local api server, otherwise use current site's origin
+const loc = window.location;
+const url = (loc.hostname === 'localhost') ? ('http://localhost:8000') : (loc.protocol + '//' + loc.host);
+
+export const socket = io(url);
 
 socket.on('connect', () => { console.log('socket connected');  });
 socket.on('disconnect', () => { console.log('socket disconnected') });
